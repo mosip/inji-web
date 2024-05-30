9d518d8899d280871c32cb54b31ad60938163a3c
Anup Nehe <anup.nehe@technoforte.co.in>
2024-05-30 15:12:12 +0530

    MOSIP-32808
    Signed-off-by: Anup Nehe <anup.nehe@technoforte.co.in>

--- inji-web-test/src/main/java/base/BasePage.java
+++ inji-web-test/src/main/java/base/BasePage.java
@@ -1,10 +1,21 @@
 package base;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
 public class BasePage {
 
-	Page page;
+	protected Page page;
 
 	public BasePage(Page page) {
 		this.page = page;
@@ -15,7 +26,33 @@ public class BasePage {
 	}
 	
 	public Boolean isElementIsVisible(String locator) {
+		 page.waitForTimeout(3000);
 		return page.locator(locator).isVisible();
 	}
+	
+	public void enterText(String locator, String text) {
+		  page.locator(locator).fill(text);
+		}
+	
+	public String getElementText(String locator) {
+		page.waitForTimeout(2000);
+		  return page.locator(locator).textContent();
+		}
+	
+	public List<String> getElementTexts(String locator) throws TimeoutException {
+		  
+		page.waitForSelector(locator, new Page.WaitForSelectorOptions().setTimeout(200));
+		List<Locator> elements = page.locator(locator).all();
+		List<String> textContents = new ArrayList<>();
+		for (Locator element : elements) {
+		  textContents.add(element.textContent());
+		}
+		return textContents;
+		
+		}
+	
+
+
+
 
 }
--- inji-web-test/src/test/java/utils/BaseTest.java
+++ inji-web-test/src/test/java/utils/BaseTest.java
@@ -1,19 +1,19 @@
 package utils;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class BaseTest{
-	
+	 
 	public static Playwright playwright;
-	public static Browser browser;
-	
+	public static BrowserContext browser;
 	DriverManager driver;
+	
 
 	public BaseTest(DriverManager driver) {
 		this.driver = driver;
@@ -21,17 +21,62 @@ public class BaseTest{
 
 	@Before
 	public void setup() {
+		String browserName = "chrome";
 		playwright = Playwright.create();
-		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-		Page page = browser.newPage();
+		Browser browser = null;
+        switch (browserName) {
+            case "chrome":
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            case "firefox":
+                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            default:
+                System.err.println("Unsupported browser: " + browserName);
+                System.exit(1);
+        }
+        Page page = browser.newPage(); 
 		driver.setPage(page);
 		driver.getPage().navigate("https://inji.qa-inji.mosip.net/");
-	}
+    }
+	
+//	@Before
+//	public void setup() throws InterruptedException {
+//	  playwright = Playwright.create();
+//
+//	  Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+//	  BrowserContext context = browser.newContext();
+//
+//	  context.route("**/*", route -> {
+//		  // Define your mock response here
+//		  route.fulfill(new Route.Handler.Response() {
+//		    @Override
+//		    public void fulfill(RouteRequest request) {
+//		      String mockResponseBody = "This is a mock response!";
+//		      route.fulfill(new Response.Builder()
+//		        .setStatus(200)  // Set the desired status code
+//		        .setHeader("Content-Type", "application/json") // Set headers if needed
+//		        .setBody(new Buffer(mockResponseBody.getBytes()))
+//		        .build());
+//		    }
+//		  });
+//		});
+//
+//	  Page page = context.newPage();
+//
+//	  // Navigate to your application page
+//	  page.navigate("https://inji.qa-inji.mosip.net/");
+//	}
+
+
+
+	
+	
 	
 	@After
 	public void tearDown() {
 		driver.getPage().close();
-		browser.close();
+//		browser.close();
 		playwright.close();
 	}
 }
