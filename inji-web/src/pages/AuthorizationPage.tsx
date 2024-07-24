46b4034c6c0feda6644892238563664b3d01ea7f
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-07-24 21:03:28 +0530

    [INJIWEB-665] : Handling Errors for OVP Data Sharing
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- inji-web/src/pages/AuthorizationPage.tsx
+++ inji-web/src/pages/AuthorizationPage.tsx
@@ -1,26 +1,47 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {SpinningLoader} from "../components/Common/SpinningLoader";
 import {ApiRequest} from "../types/data";
 import {api} from "../utils/api";
-import {useFetch} from "../hooks/useFetch";
-export const AuthorizationPage:React.FC = () => {
+import {RequestStatus, useFetch} from "../hooks/useFetch";
+import {ErrorSheildIcon} from "../components/Common/ErrorSheildIcon";
+import {LandingPageWrapper} from "../components/Common/LandingPageWrapper";
+import {useTranslation} from "react-i18next";
 
-    const {fetchRequest} = useFetch();
+export const AuthorizationPage: React.FC = () => {
+
+    const {fetchRequest, state} = useFetch();
+    const [response, setResponse] = useState<any>();
+    const {t} = useTranslation("RedirectionPage");
+
+    async function authorizePresentation() {
+        try {
+            const queryParams = new URLSearchParams(window.location.search)
+            const responseType = queryParams.get("response_type") + "";
+            const resource = queryParams.get("resource") + "";
+            const clientId = queryParams.get("client_id") + "";
+            const redirectUri = queryParams.get("redirect_uri") + "";
+            const presentationDefinition = queryParams.get("presentation_definition") + "";
+
+            const apiRequest: ApiRequest = api.presentationAuthorization;
+            const apiResponse = await fetchRequest(
+                apiRequest.url(responseType, resource, clientId, redirectUri, presentationDefinition),
+                apiRequest.methodType,
+                apiRequest.headers()
+            );
+            setResponse(apiResponse);
+            console.log("Presentation Authorized Successfully");
+        } catch (e) {
+            console.error("Exception occurred while invoking the authorization"+e);
+        }
+    }
+
     useEffect( () => {
-        const queryParams = new URLSearchParams(window.location.search)
-        const responseType = queryParams.get("response_type") + "";
-        const resource = queryParams.get("resource") + "";
-        const clientId = queryParams.get("client_id") + "";
-        const redirectUri = queryParams.get("redirect_uri") + "";
-        const presentationDefinition = queryParams.get("presentation_definition") + "";
-
-        const apiRequest: ApiRequest = api.presentationAuthorization;
-        fetchRequest(
-            apiRequest.url(responseType, resource, clientId, redirectUri, presentationDefinition),
-            apiRequest.methodType,
-            apiRequest.headers()
-        );
+        authorizePresentation();
     },[])
 
-    return <SpinningLoader />
+    return <React.Fragment>
+        {state === RequestStatus.DONE && <LandingPageWrapper icon={<ErrorSheildIcon />} title={response.errors[0].error} subTitle={response.errors[0].error_description} gotoHome={false}/> }
+        {state === RequestStatus.LOADING && <LandingPageWrapper icon={<SpinningLoader />} title={""} subTitle={""} gotoHome={false}/> }
+        {state === RequestStatus.ERROR && <LandingPageWrapper icon={<ErrorSheildIcon />} title={t("error.generic.title")} subTitle={t("error.generic.subTitle")} gotoHome={false}/> }
+    </React.Fragment>
 }
