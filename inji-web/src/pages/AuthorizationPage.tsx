9cb361ca08c1bad4cf352dea76223dd4bb925d7c
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-07-25 19:52:09 +0530

    [INJIWEB-665] : handle the errors in injiweb using redirect
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- inji-web/src/pages/AuthorizationPage.tsx
+++ inji-web/src/pages/AuthorizationPage.tsx
@@ -1,11 +1,23 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {SpinningLoader} from "../components/Common/SpinningLoader";
 import {api} from "../utils/api";
 import {LandingPageWrapper} from "../components/Common/LandingPageWrapper";
+import {ErrorSheildIcon} from "../components/Common/ErrorSheildIcon";
 
 export const AuthorizationPage: React.FC = () => {
+    const [error, setError] = useState<String>("");
+    const [errorDesc, setErrorDesc] = useState<String>("");
     useEffect( () => {
-        window.location.href = api.mimotoHost + "/authorize" + window.location.search;
-    },[])
-    return <LandingPageWrapper icon={<SpinningLoader />} title={""} subTitle={""} gotoHome={false}/>
+        const currentQueryParams = new URLSearchParams(window.location.search);
+        setError(currentQueryParams.get("error")+"");
+        setErrorDesc(currentQueryParams.get("error_description")+"")
+        if(error == "null"){
+            window.location.href = api.mimotoHost + "/authorize" + window.location.search;
+        }
+
+    },[error])
+    if(error == ""){
+        return <div><LandingPageWrapper icon={<SpinningLoader/>} title={""} subTitle={""} gotoHome={false}/></div>
+    }
+    return <div><LandingPageWrapper icon={<ErrorSheildIcon />} title={error+""} subTitle={errorDesc+""} gotoHome={false}/></div>
 }
