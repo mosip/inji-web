69833cb9a67b7df7662e80d0d7e70fd19c263af3
Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>
2024-10-24 11:40:54 +0530

    [INJIWEB-1000]: Fix Hamburger Not-Closing
    Signed-off-by: Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>

--- inji-web/src/components/PageTemplate/Header.tsx
+++ inji-web/src/components/PageTemplate/Header.tsx
@@ -17,12 +17,12 @@ export const Header: React.FC = () => {
                 className="fixed top-0 left-0 right-0 bg-iw-background py-7 shadow-sm">
             <div className="container mx-auto flex justify-between items-center px-4">
                 <div data-testid="Header-InjiWeb-Logo-Container" className={"flex flex-row space-x-9 justify-center items-center"}>
-                    <div  role="button" tabIndex={0} className={"sm:hidden"} onClick={() => setIsOpen(open => !open)} onKeyUp={() => setIsOpen(open => !open)}>
+                    <div  role="button" tabIndex={0} className={"sm:hidden"} onMouseDown={() => setIsOpen(open => !open)} onKeyUp={() => setIsOpen(open => !open)}>
                         <GiHamburgerMenu size={32}/>
                     </div>
                     <div role={"button"}
                          tabIndex={0}
-                         onClick={() => navigate("/")}
+                         onMouseDown={() => navigate("/")}
                          onKeyUp={() => navigate("/")}>
                         <img src={require("../../assets/InjiWebLogo.png")}
                              className={"h-13 w-28 scale-150 cursor-pointer"}
@@ -39,7 +39,7 @@ export const Header: React.FC = () => {
                         </li>
                         <li data-testid="Header-Menu-Help">
                             <div data-testid="Header-Menu-Help-div"
-                                 onClick={() => navigate("/help") }
+                                 onMouseDown={() => navigate("/help") }
                                  onKeyUp={() => navigate("/help") }
                                  role="button"
                                  tabIndex={0}
@@ -54,21 +54,21 @@ export const Header: React.FC = () => {
                     <div className="container mx-auto px-4 flex flex-col justify-start items-start font-semibold"
                          role="button"
                          tabIndex={0}
-                         onClick={() => setIsOpen(false)}
+                         onMouseDown={() => setIsOpen(false)}
                          onBlur={() => setIsOpen(false)}>
                         <div data-testid="Header-Menu-AboutInji"
                              className={"py-5 w-full"}
                              role="button"
                              tabIndex={0}
                              onKeyUp={() => {window.open("https://docs.mosip.io/inji/inji-web/overview", "_blank","noopener");setIsOpen(false)}}
-                             onClick={() => {window.open("https://docs.mosip.io/inji/inji-web/overview", "_blank","noopener");setIsOpen(false)}}>
+                             onMouseDown={() => {window.open("https://docs.mosip.io/inji/inji-web/overview", "_blank","noopener");setIsOpen(false)}}>
                             {t("Header.aboutInji")}
                         </div>
                         <div data-testid="Header-Menu-Help"
                              role="button"
                              tabIndex={0}
                              onKeyUp={() => {navigate("/help");setIsOpen(false)}}
-                             onClick={() => {navigate("/help");setIsOpen(false)}}
+                             onMouseDown={() => {navigate("/help");setIsOpen(false)}}
                              className="text-iw-title cursor-pointer py-5 w-full inline-block sm:hidden">
                                 {t("Header.help")}
                         </div>
