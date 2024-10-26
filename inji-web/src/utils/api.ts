f4ea9c5dba27043b47f07e13f07c7a726cb34aac
Hitesh C <jainhitesh9998@gmail.com>
2024-10-26 17:21:02 +0530

    [INJICERT-567]
    Signed-off-by: Hitesh C <jainhitesh9998@gmail.com>

--- inji-web/src/utils/api.ts
+++ inji-web/src/utils/api.ts
@@ -14,7 +14,8 @@ export enum MethodType {
 export class api {
 
     // static mimotoHost = "http://localhost:3010";
-     static mimotoHost = window.location.origin + "/v1/mimoto";
+    //  static mimotoHost = window.location.origin + "/v1/mimoto";
+    static mimotoHost = process.env.REACT_APP_MIMOTO_HOST;
 
     static authorizationRedirectionUrl = window.location.origin + "/redirect";
 
