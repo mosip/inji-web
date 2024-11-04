71ebc8e76e4b654e6ffb4c71791ad579f2f1e89d
SAIRAM GIRIRAO <sairam.girirao@infosys.com>
2024-11-04 18:56:35 +0530

    [INJIWEB-511]:Added background img for Homebanner
    Signed-off-by: SAIRAM GIRIRAO <sairam.girirao@infosys.com>

--- inji-web/src/components/Home/HomeBanner.tsx
+++ inji-web/src/components/Home/HomeBanner.tsx
@@ -6,7 +6,10 @@ export const HomeBanner: React.FC<HomeBannerProps> = (props) => {
     const {t} = useTranslation("HomePage");
     return (
         <div data-testid="HomeBanner-Container" className="py-2 pb-10">
-            <div data-testid="HomeBanner-Content" className="mt-8 sm:mx-[8%] md:rounded-xl pt-14 pb-7 sm:py-20 flex flex-col justify-center items-center bg-gradient-to-r from-iw-primary to-iw-secondary h-1/6">
+            <div 
+                data-testid="HomeBanner-Content" 
+                className="mt-8 sm:mx-[8%] sm:rounded-xl pt-14 pb-7 sm:py-20 flex flex-col justify-center items-center bg-home-banner h-1/6"
+            >
                 <span data-testid="HomeBanner-Heading" className="text-4xl sm:text-5xl text-iw-text font-semibold text-wrap w-[91%] sm:w-[66%] text-center pb-5">
                     {t("Banner.heading")}
                 </span>
