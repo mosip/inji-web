91c23b1bcbe9b5d1e8ff9ba399689777d9cdeebf
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-11-05 10:59:19 +0530

    [INJIWEB-511]: update the data-testids
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- inji-web/src/components/Home/HomeFeatureItem.tsx
+++ inji-web/src/components/Home/HomeFeatureItem.tsx
@@ -2,43 +2,43 @@ import React from "react";
 import {FaRegCheckCircle} from "react-icons/fa";
 import {GradientWrapper} from "../Common/GradientWrapper";
 import { useTranslation } from "react-i18next";
-import { HomeFeatureItemProps } from "../../types/components"; 
+import { HomeFeatureItemProps } from "../../types/components";
 
 export const HomeFeatureItem: React.FC<HomeFeatureItemProps> = (props) => {
   const {t} = useTranslation("HomePage");
 
   return (
-    <div data-testid="HomeFeatureItem-Container" className="bg-gray-50 p-7 max-w-96 shadow-sm">
-      <img data-testid="HomeFeatureItem-Image" src={require("../../assets/FeatureItem" + props.itemno + ".svg")} alt="feature item" />
-      <div data-testid="HomeFeatureItem-Heading" className="font-semibold text-black text-xl text-wrap py-7">
+    <div data-testid={"HomeFeatureItem" + props.itemno + "-Container"} className="bg-gray-50 p-7 max-w-96 shadow-sm">
+      <img data-testid={"HomeFeatureItem" + props.itemno + "-Image"} src={require("../../assets/FeatureItem" + props.itemno + ".svg")} alt="feature item" />
+      <div data-testid={"HomeFeatureItem" + props.itemno + "-Heading"} className="font-semibold text-black text-xl text-wrap py-7">
         {t("FeatureItem" + props.itemno + ".heading")}
       </div>
-      <div data-testid="HomeFeatureItem-FirstFeature" className="flex flex-row container mx-auto py-2">
+      <div data-testid={"HomeFeatureItem" + props.itemno + "-FirstFeature"} className="flex flex-row container mx-auto py-2">
         <div className="pe-3">
           <GradientWrapper>
             <FaRegCheckCircle size={20} />
           </GradientWrapper>
         </div>
         <div className="flex flex-col">
-          <span data-testid="HomeFeatureItem-FirstFeature-Item" className="font-medium">
+          <span data-testid={"HomeFeatureItem" + props.itemno + "-FirstFeature-Item"} className="font-medium">
             {t("FeatureItem" + props.itemno + ".item1")}
           </span>
-          <span data-testid="HomeFeatureItem-FirstFeature-Description" className="font-extralight">
+          <span data-testid={"HomeFeatureItem" + props.itemno + "-FirstFeature-Description"} className="font-extralight">
             {t("FeatureItem" + props.itemno + ".description1")}
           </span>
         </div>
       </div>
-      <div data-testid="HomeFeatureItem-SecondFeature" className="flex flex-row container mx-auto mt-10">
+      <div data-testid={"HomeFeatureItem" + props.itemno + "-SecondFeature"} className="flex flex-row container mx-auto mt-10">
         <div className="pe-3">
           <GradientWrapper>
             <FaRegCheckCircle size={20} />
           </GradientWrapper>
         </div>
         <div className="flex flex-col">
-          <span data-testid="HomeFeatureItem-SecondFeature-Item" className="font-medium">
+          <span data-testid={"HomeFeatureItem" + props.itemno + "-SecondFeature-Item"} className="font-medium">
             {t("FeatureItem" + props.itemno + ".item2")}
           </span>
-          <span data-testid="HomeFeatureItem-SecondFeature-Description" className="font-extralight">
+          <span data-testid={"HomeFeatureItem" + props.itemno + "-SecondFeature-Description"} className="font-extralight">
             {t("FeatureItem" + props.itemno + ".description2")}
           </span>
         </div>
