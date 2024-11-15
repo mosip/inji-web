eac5aede59a50352bd7436e954b2b19894b2bf65
SAIRAM GIRIRAO <sairam.girirao@infosys.com>
2024-11-15 13:50:42 +0530

    [INJIWEB-1056] Change CAR theme
    Signed-off-by: SAIRAM GIRIRAO <sairam.girirao@infosys.com>

--- inji-web/src/components/PageTemplate/Footer.tsx
+++ inji-web/src/components/PageTemplate/Footer.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import {useTranslation} from "react-i18next";
 
 export const Footer: React.FC = () => {
-
     const {t} = useTranslation("PageTemplate")
+    const logo = require('../../assets/InjiWebLogoonly.png'); // Adjust the path to your image
 
     return <footer
-        data-testid="Footer-Container"
-        className="fixed bottom-0 left-0 right-0 py-4 text-center transform rotate-180 shadow-sm shadow-iw-shadow bg-iw-footer">
-        <div className="container mx-auto">
-            <p data-testid="Footer-Text" className="text-black transform rotate-180">{t("Footer.copyRight")}</p>
-        </div>
-    </footer>;
-}
+            data-testid="Footer-Container"
+            className="fixed bottom-0 left-0 right-0 py-6 pl-4 shadow-sm shadow-iw-shadow bg-iw-footer"
+        >
+            <div className="container mx-12 flex items-center">
+                <img src={logo} alt="Logo" className="h-8 mr-2" />
+                <p data-testid="Footer-Text" className="text-gray-500 font-semibold">{t("Footer.copyRight")}</p>
+            </div>
+        </footer>
+};
--- inji-web/src/components/PageTemplate/Header.tsx
+++ inji-web/src/components/PageTemplate/Header.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
-import {useTranslation} from "react-i18next";
-import {useNavigate} from "react-router-dom";
-import {LanguageSelector} from "../Common/LanguageSelector";
-import {GiHamburgerMenu} from "react-icons/gi";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { LanguageSelector } from "../Common/LanguageSelector";
+import { GiHamburgerMenu } from "react-icons/gi";
 import OutsideClickHandler from 'react-outside-click-handler';
 
 export const Header: React.FC = () => {
@@ -13,71 +13,77 @@ export const Header: React.FC = () => {
 
     return (
         <header>
-        <div data-testid="Header-Container"
-                className="fixed top-0 left-0 right-0 bg-iw-background py-4 shadow-sm">
-            <div className="container mx-auto flex justify-between items-center px-4">
-                <div data-testid="Header-InjiWeb-Logo-Container" className={"flex flex-row justify-center items-center"}>
-                    <div  role="button" tabIndex={0} className={"sm:hidden"} onClick={() => setIsOpen(open => !open)} onKeyUp={() => setIsOpen(open => !open)}>
-                        <GiHamburgerMenu size={32}/>
+            <div
+                data-testid="Header-Container"
+                className="fixed top-0 left-0 right-0 bg-iw-background py-7 shadow-sm"
+            >
+                <div className="container mx-auto flex justify-between items-center px-4">
+                    <div data-testid="Header-InjiWeb-Logo-Container" className={"flex flex-row space-x-9 justify-center items-center"}>
+                        <div role="button" tabIndex={0} className="sm:hidden" onMouseDown={() => setIsOpen(open => !open)} onKeyUp={() => setIsOpen(open => !open)}>
+                            <GiHamburgerMenu size={32} />
+                        </div>
+                        <div role="button"
+                            tabIndex={0}
+                            onMouseDown={() => navigate("/")}
+                            onKeyUp={() => navigate("/")}>
+                            <img src={require("../../assets/Cadastro Ambiental Rural 2.png")}
+                                className={"h-13 w-28 scale-150 cursor-pointer"}
+                                data-testid="Header-InjiWeb-Logo"
+                                alt="CAR Logo"/>
+                        </div>
                     </div>
-                    <div role={"button"}
-                         tabIndex={0}
-                         onClick={() => navigate("/")}
-                         onKeyUp={() => navigate("/")}>
-                        <img src={require("../../assets/InjiWebLogo.png")}
-                             className={"h-13 w-28 cursor-pointer"}
-                             data-testid="Header-InjiWeb-Logo"
-                             alt="Inji Web Logo"/>
-                    </div>
+                    <nav>
+                        <ul className="flex space-x-10 items-center font-semibold" data-testid="Header-Menu-Elements">
+                            <li data-testid="Header-Menu-Home">
+                                <div
+                                    data-testid="Header-Menu-Home-div"
+                                    onMouseDown={() => navigate("/")}
+                                    onKeyUp={() => navigate("/")}
+                                    role="button"
+                                    tabIndex={0}
+                                    className="text-iw-title cursor-pointer hidden sm:inline-block">
+                                    {t("Header.home")}
+                                </div>
+                            </li>
+                            <li data-testid="Header-Menu-Help">
+                                <div
+                                    data-testid="Header-Menu-Help-div"
+                                    onMouseDown={() => navigate("/help")}
+                                    onKeyUp={() => navigate("/help")}
+                                    role="button"
+                                    tabIndex={0}
+                                    className="text-iw-title items-center cursor-pointer hidden sm:inline-block"
+                                >
+                                    {t("Header.help")}
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div  className={"font-semibold"} data-testid="Header-Menu-LanguageSelector">
+                                <LanguageSelector />
+                            </div>
                 </div>
-                <nav>
-                    <ul className="flex space-x-4 items-center font-semibold" data-testid="Header-Menu-Elements">
-                        <li data-testid="Header-Menu-AboutInji"><a href="https://docs.mosip.io/inji/inji-web/overview"
-                                                                   target="_blank"
-                                                                   rel="noreferrer"
-                                                                   className="text-iw-title hidden sm:inline-block">{t("Header.aboutInji")}</a>
-                        </li>
-                        <li data-testid="Header-Menu-Help">
-                            <div data-testid="Header-Menu-Help-div"
-                                 onClick={() => navigate("/help") }
-                                 onKeyUp={() => navigate("/help") }
-                                 role="button"
-                                 tabIndex={0}
-                                 className="text-iw-title cursor-pointer hidden sm:inline-block">{t("Header.help")}</div>
-                        </li>
-                        <li data-testid="Header-Menu-LanguageSelector"><LanguageSelector/></li>
-                    </ul>
-                </nav>
-            </div>
-            { isOpen &&
-                <OutsideClickHandler onOutsideClick={()=>setIsOpen(false)} >
-                    <div className="container mx-auto px-4 flex flex-col justify-start items-start font-semibold"
-                         role="button"
-                         tabIndex={0}
-                         onMouseDown={() => setIsOpen(false)}
-                         onBlur={() => setIsOpen(false)}>
-                        <div data-testid="Header-Menu-AboutInji"
-                             className={"py-5 w-full"}
-                             role="button"
-                             tabIndex={0}
-                             onKeyUp={() => {window.open("https://docs.mosip.io/inji/inji-web/overview", "_blank","noopener");setIsOpen(false)}}
-                             onClick={() => {window.open("https://docs.mosip.io/inji/inji-web/overview", "_blank","noopener");setIsOpen(false)}}>
-                            {t("Header.aboutInji")}
-                        </div>
-                        <div data-testid="Header-Menu-Help"
-                             role="button"
-                             tabIndex={0}
-                             onKeyUp={() => {navigate("/help");setIsOpen(false)}}
-                             onClick={() => {navigate("/help");setIsOpen(false)}}
-                             className="text-iw-title cursor-pointer py-5 w-full inline-block sm:hidden">
+                {isOpen && (
+                    <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}>
+                        <div
+                            className="container sm:hidden mx-auto px-4 flex flex-col justify-start items-start font-semibold"
+                            role="button"
+                            tabIndex={0}
+                            onMouseDown={() => setIsOpen(false)}
+                            onBlur={() => setIsOpen(false)}>
+                            <div
+                                data-testid="Header-Menu-Help"
+                                role="button"
+                                tabIndex={0}
+                                onKeyUp={() => { navigate("/help"); setIsOpen(false); }}
+                                onMouseDown={() => { navigate("/help"); setIsOpen(false); }}
+                                className="text-iw-title cursor-pointer py-5 w-full inline-block">
                                 {t("Header.help")}
+                            </div>
                         </div>
-                    </div>
-                </OutsideClickHandler>
-            }
-        </div>
+                    </OutsideClickHandler>
+                )}
+            </div>
         </header>
-    )
-
-}
-
+    );
+};
