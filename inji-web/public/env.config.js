bd94866906d1d2eea3cb775c2e56e8b77450c294
Sairam Girirao <105053210+Sairam-g9162@users.noreply.github.com>
2024-11-27 13:47:03 +0530

    [INJIWEB_1106]: Update env.config.js
    Signed-off-by: Sairam Girirao <105053210+Sairam-g9162@users.noreply.github.com>

--- inji-web/public/env.config.js
+++ inji-web/public/env.config.js
@@ -2,6 +2,6 @@ window._env_ = {
     DEFAULT_LANG: "en",
     DEFAULT_THEME: "CAR_theme",
     DEFAULT_FAVICON: "favicon.ico",
-    DEFAULT_TITLE: "Inji Web",
+    DEFAULT_TITLE: "CAR",
     DEFAULT_FONT_URL: "https://fonts.googleapis.com/css?family=Inter"
 };
