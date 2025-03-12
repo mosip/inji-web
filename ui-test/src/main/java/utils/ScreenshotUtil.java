d8427805aa45dbe5aecec0979eeab9140920d677
Anup Nehe <anup.nehe@technoforte.co.in>
2025-03-12 07:43:38 +0530

    resolve
    Signed-off-by: Anup Nehe <anup.nehe@technoforte.co.in>

--- ui-test/src/main/java/utils/ScreenshotUtil.java
+++ ui-test/src/main/java/utils/ScreenshotUtil.java
@@ -12,22 +12,22 @@ import utils.ExtentReportManager;
 
 public class ScreenshotUtil  {
 
-	public static String captureScreenshot(WebDriver driver, String screenshotName) throws IOException {
-	    if (driver == null) {
-	        System.err.println("WebDriver instance is null. Cannot take a screenshot.");
-	        throw new IllegalStateException("WebDriver instance is null.");
-	    }
-	    TakesScreenshot ts = (TakesScreenshot) driver;
-	    File source = ts.getScreenshotAs(OutputType.FILE);
-	    
-	    // Save the screenshot
-	    String destinationPath = System.getProperty("user.dir") + "/screenshots/" + screenshotName + "_" + System.currentTimeMillis() + ".png";
-	    File destination = new File(destinationPath);
-	    destination.getParentFile().mkdirs();
-	    FileUtils.copyFile(source, destination);
+    public static String captureScreenshot(WebDriver driver, String screenshotName) throws IOException {
+        if (driver == null) {
+            System.err.println("WebDriver instance is null. Cannot take a screenshot.");
+            throw new IllegalStateException("WebDriver instance is null.");
+        }
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
 
-	    return destinationPath;
-	}
+        String destinationPath = System.getProperty("user.dir") + "/screenshots/" + screenshotName + "_" + System.currentTimeMillis() + ".png";
+        System.err.println("destinationpath:-"+destinationPath);
+        File destination = new File(destinationPath);
+        destination.getParentFile().mkdirs();
+        FileUtils.copyFile(source, destination);
+
+        return destinationPath;
+    }
 
     public static void attachScreenshot(WebDriver driver, String screenshotName) {
         try {
@@ -35,8 +35,8 @@ public class ScreenshotUtil  {
 
             File screenshotFile = new File(screenshotPath);
             if (screenshotFile.exists()) {
-                ExtentReportManager.getTest().info("Screenshot Captured", 
-                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+                ExtentReportManager.getTest().info("Screenshot Captured",
+                        MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
             } else {
                 ExtentReportManager.getTest().warning("Screenshot file not found: " + screenshotPath);
             }
