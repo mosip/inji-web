32b7f25ac472f88bc5e4c260b4bbda458fa6bd00
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-03-19 00:28:57 +0530

    [INJIWEB-1379] receive and store the error message of any error occured during the database connection or while storing the user data
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/pages/users/login/Login.tsx
+++ inji-web/src/pages/users/login/Login.tsx
@@ -24,7 +24,7 @@ const Login: React.FC = () => {
             fetchUserProfile();
         } else if (status === "error") {
             setIsLoading(false);
-            setError("Login failed");
+            setError(params.get("error_message"));
         }
     }, [navigate]);
 
