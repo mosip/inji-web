5721fb5dce9e1f7a4937844894886e40a9cbb607
Rakshithb1 <rakshit.b@technoforte.co.in>
2025-04-16 17:42:22 +0530

    [MOSIP-40792] updated keys name
    Signed-off-by: Rakshithb1 <rakshit.b@technoforte.co.in>

--- deploy/install.sh
+++ deploy/install.sh
@@ -12,41 +12,41 @@ DATASHARE_CHART_VERSION=1.3.0-beta.2-develop
 ##The value of INJI_DATASHARE_HOST is set to a fixed value: "datashare-inji.injiweb".
 INJI_DATASHARE_HOST="datashare-inji.injiweb"
 
-DEFAULT_MOSIP_INJIWEB_HOST=$( kubectl get cm inji-stack-config -n config-server -o jsonpath={.data.mosip-injiweb-host} )
-# Check if MOSIP_INJIWEB_HOST is present under configmap/inji-stack-config of configserver
-if echo "$DEFAULT_MOSIP_INJIWEB_HOST" | grep -q "MOSIP_INJIWEB_HOST"; then
-    echo "MOSIP_INJIWEB_HOST is already present in configmap/inji-stack-config of configserver"
-    MOSIP_INJIWEB_HOST=DEFAULT_MOSIP_INJIWEB_HOST
+DEFAULT_INJIWEB_HOST=$( kubectl get cm inji-stack-config -n config-server -o jsonpath={.data.injiweb-host} )
+# Check if INJIWEB_HOST is present under configmap/inji-stack-config of configserver
+if echo "$DEFAULT_INJIWEB_HOST" | grep -q "INJIWEB_HOST"; then
+    echo "INJIWEB_HOST is already present in configmap/inji-stack-config of configserver"
+    MOSIP_INJIWEB_HOST=DEFAULT_INJIWEB_HOST
 else
-    read -p "Please provide injiwebhost (eg: injiweb.sandbox.xyz.net ) : " MOSIP_INJIWEB_HOST
+    read -p "Please provide injiwebhost (eg: injiweb.sandbox.xyz.net ) : " INJIWEB_HOST
 
-    if [ -z "MOSIP_INJIWEB_HOST" ]; then
+    if [ -z "INJIWEB_HOST" ]; then
     echo "INJIWEB Host not provided; EXITING;"
     exit 0;
     fi
 fi
 
-CHK_MOSIP_INJIWEB_HOST=$( nslookup "$MOSIP_INJIWEB_HOST" )
+CHK_INJIWEB_HOST=$( nslookup "$INJIWEB_HOST" )
 if [ $? -gt 0 ]; then
     echo "Injiweb Host does not exists; EXITING;"
     exit 0;
 fi
 
-echo "MOSIP_INJIWEB_HOST is not present in configmap/inji-stack-config of configserver"
+echo "INJIWEB_HOST is not present in configmap/inji-stack-config of configserver"
     # Add injiweb host to inji-stack-config
-    kubectl patch configmap inji-stack-config -n config-server --type merge -p "{\"data\": {\"mosip-injiweb-host\": \"$MOSIP_INJIWEB_HOST\"}}"
-    kubectl patch configmap inji-stack-config -n default --type merge -p "{\"data\": {\"mosip-injiweb-host\": \"$MOSIP_INJIWEB_HOST\"}}"
+    kubectl patch configmap inji-stack-config -n config-server --type merge -p "{\"data\": {\"injiweb-host\": \"$INJIWEB_HOST\"}}"
+    kubectl patch configmap inji-stack-config -n default --type merge -p "{\"data\": {\"injiweb-host\": \"$INJIWEB_HOST\"}}"
     # Add the host
-    kubectl -n config-server set env --keys=mosip-injiweb-host --from configmap/inji-stack-config deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
+    kubectl -n config-server set env --keys=injiweb-host --from configmap/inji-stack-config deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
     # Restart the configserver deployment
     kubectl -n config-server get deploy -o name | xargs -n1 -t kubectl -n config-server rollout status
 
-DEFAULT_INJI_DATASHARE_HOST=$(kubectl get cm inji-stack-config -n config-server -o jsonpath={.data.mosip-inji-datashare-host})
+DEFAULT_INJI_DATASHARE_HOST=$(kubectl get cm inji-stack-config -n config-server -o jsonpath={.data.inji-datashare-host})
 if [ -z "$DEFAULT_INJI_DATASHARE_HOST" ]; then
     echo "Adding INJI_DATASHARE_HOST to config-server deployment"
-    kubectl patch configmap inji-stack-config -n config-server --type merge -p "{\"data\": {\"mosip-inji-datashare-host\": \"$INJI_DATASHARE_HOST\"}}"
-    kubectl patch configmap inji-stack-config -n default --type merge -p "{\"data\": {\"mosip-inji-datashare-host\": \"$INJI_DATASHARE_HOST\"}}"
-    kubectl -n config-server set env --keys=mosip-inji-datashare-host --from configmap/inji-stack-config deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
+    kubectl patch configmap inji-stack-config -n config-server --type merge -p "{\"data\": {\"inji-datashare-host\": \"$INJI_DATASHARE_HOST\"}}"
+    kubectl patch configmap inji-stack-config -n default --type merge -p "{\"data\": {\"inji-datashare-host\": \"$INJI_DATASHARE_HOST\"}}"
+    kubectl -n config-server set env --keys=inji-datashare-host --from configmap/inji-stack-config deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
     kubectl -n config-server get deploy -o name | xargs -n1 -t kubectl -n config-server rollout status
 fi
 
@@ -62,18 +62,18 @@ helm repo update
 
 ./copy_cm.sh
 
-INJI_DATASHARE_HOST=$(kubectl get cm inji-stack-config -o jsonpath={.data.mosip-inji-datashare-host})
+INJI_DATASHARE_HOST=$(kubectl get cm inji-stack-config -o jsonpath={.data.inji-datashare-host})
 echo "Installing datashare"
 helm -n $NS install datashare-inji mosip/datashare \
   -f datashare-values.yaml \
   --version $DATASHARE_CHART_VERSION
 
-INJI_HOST=$(kubectl get cm inji-stack-config -o jsonpath={.data.mosip-injiweb-host})
+INJI_HOST=$(kubectl get cm inji-stack-config -o jsonpath={.data.injiweb-host})
 echo "Installing INJIWEB"
 helm -n $NS install injiweb mosip/injiweb \
   -f values.yaml \
-  --set inji_web.configmaps.injiweb-ui.MIMOTO_HOST=https://$MOSIP_INJIWEB_HOST/v1/mimoto \
-  --set istio.hosts[0]=$MOSIP_INJIWEB_HOST \
+  --set inji_web.configmaps.injiweb-ui.MIMOTO_HOST=https://$INJIWEB_HOST/v1/mimoto \
+  --set istio.hosts[0]=$INJIWEB_HOST \
   --version $CHART_VERSION
 
 kubectl -n $NS get deploy -o name | xargs -n1 -t kubectl -n $NS rollout status
