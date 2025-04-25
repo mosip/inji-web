32aef338d4bbf9ce542f048fe4d4889aa9253389
Anup Nehe <anup.nehe@technoforte.co.in>
2025-04-25 12:57:55 +0530

    resolve conflicts
    Signed-off-by: Anup Nehe <anup.nehe@technoforte.co.in>

--- ui-test/src/main/java/runnerfiles/Runner.java
+++ ui-test/src/main/java/runnerfiles/Runner.java
@@ -26,7 +26,7 @@ import java.util.List;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"/home/mosip/featurefiles/downloadMosipCredentials.feature"},
+		features = {"/home/mosip/featurefiles/"},
 		dryRun = false,
 		glue = {"stepdefinitions", "utils"},
 		snippets = SnippetType.CAMELCASE,
