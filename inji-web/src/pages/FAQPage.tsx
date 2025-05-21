56be6258b6ba9dbbe919ed970995deca3b3c870c
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-05-21 16:52:14 +0530

    [INJIWEB-1513] remove the data test id in Faq accordian parent div
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/pages/FAQPage.tsx
+++ inji-web/src/pages/FAQPage.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import {FAQAccordion} from "../components/Faq/FAQAccordion";
-import {useTranslation} from "react-i18next";
-import {useLocation, useNavigate} from "react-router-dom";
-import { FAQPageProps } from "../components/Dashboard/types";
+import React from 'react';
+import {FAQAccordion} from '../components/Faq/FAQAccordion';
+import {useTranslation} from 'react-i18next';
+import {useLocation, useNavigate} from 'react-router-dom';
+import {FAQPageProps} from '../components/Dashboard/types';
 
 export const FAQPage: React.FC<FAQPageProps> = ({backUrl, withHome}) => {
-    const {t} = useTranslation(["FAQPage", "Dashboard"]);
+    const {t} = useTranslation(['FAQPage', 'Dashboard']);
     const navigate = useNavigate();
     const location = useLocation();
     const previousPagePath = location.state?.from;
@@ -16,14 +16,12 @@ export const FAQPage: React.FC<FAQPageProps> = ({backUrl, withHome}) => {
         } else if (previousPagePath) {
             navigate(previousPagePath); // Navigate to the previous link in history
         } else {
-            navigate("/dashboard/home"); // Navigate to homepage if opened directly
+            navigate('/dashboard/home'); // Navigate to homepage if opened directly
         }
     };
 
     return (
-        <div
-            className="w-full flex flex-col sm:flex-row justify-between items-start p-1 sm:p-2 md:p-4 flex-grow overflow-y-auto"
-        >
+        <div className="w-full flex flex-col sm:flex-row justify-between items-start p-1 sm:p-2 md:p-4 flex-grow overflow-y-auto">
             <div className="flex flex-row items-start gap-4 w-full">
                 <div className="flex items-start">
                     <svg
@@ -44,21 +42,18 @@ export const FAQPage: React.FC<FAQPageProps> = ({backUrl, withHome}) => {
                 <div className="flex flex-col items-start gap-8 w-full mr-2 sm:mr-4 md:mr-8 lg:mr-10">
                     <div className="flex flex-col items-start">
                         <span className="text-2xl font-medium">
-                            {t("title")}
+                            {t('title')}
                         </span>
                         {withHome && (
                             <span
                                 className="text-xs sm:text-sm text-[#5B03AD] cursor-pointer hover:underline"
-                                onClick={() => navigate("/dashboard/home")}
+                                onClick={() => navigate('/dashboard/home')}
                             >
-                                {t("Dashboard:Home.title")}
+                                {t('Dashboard:Home.title')}
                             </span>
                         )}
                     </div>
-                    <div
-                        data-testid="Faq-Accordion-Container"
-                        className="w-full"
-                    >
+                    <div className="w-full">
                         <FAQAccordion />
                     </div>
                 </div>
