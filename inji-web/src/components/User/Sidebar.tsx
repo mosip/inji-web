af907de7bd67ec3e6cb27326c9e9d5f155f37b61
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-06-05 16:14:45 +0530

    [INJIWEB-1541] update TODO
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- inji-web/src/components/User/Sidebar.tsx
+++ inji-web/src/components/User/Sidebar.tsx
@@ -80,7 +80,7 @@ export const Sidebar: React.FC = () => {
 
     const sidebarItems: SidebarItemType[] = [
         {
-            // TODO: can the consumer assign same route navigation to icon and text and all?
+            // TODO: Can we avoid usage of ROUTES.USER_HOME linking in two places?
             icon: (
                 <SideBarSvgIcon
                     outline="M3 12L5 10M5 10L12 3L19 10M5 10V20C5 20.5523 5.44772 21 6 21H9M19 10L21 12M19 10V20C19 20.5523 18.5523 21 18 21H15M9 21C9.55228 21 10 20.5523 10 20V16C10 15.4477 10.4477 15 11 15H13C13.5523 15 14 15.4477 14 16V20C14 20.5523 14.4477 21 15 21M9 21H15"
