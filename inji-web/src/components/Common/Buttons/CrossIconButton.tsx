4309a0dd1051641a29f67998e772b5096571c2a1
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-06-12 10:50:14 +0530

    [INJIWEB-1557] remove aria-hidden set to true for svg image in CrossIconButton as we want the screen readers to read it out loud
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/components/Common/Buttons/CrossIconButton.tsx
+++ inji-web/src/components/Common/Buttons/CrossIconButton.tsx
@@ -26,7 +26,6 @@ export const CrossIconButton: React.FC<CrossIconButtonProps> = ({
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                aria-hidden="true"
                 className={iconClassName}
                 data-testid={iconTestId}
             >
