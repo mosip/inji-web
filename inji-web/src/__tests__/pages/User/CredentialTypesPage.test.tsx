ecd4093f597f92d120cda49ec42be22f581c18ab
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-06-25 17:39:26 +0530

    [INJIWEB-1592] use useApi in CredentialTypesPage
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- inji-web/src/__tests__/pages/User/CredentialTypesPage.test.tsx
+++ inji-web/src/__tests__/pages/User/CredentialTypesPage.test.tsx
@@ -1,74 +1,84 @@
-import { renderWithRouter } from '../../../test-utils/mockUtils';
-import { screen, waitFor } from '@testing-library/react';
-import { useFetch, RequestStatus } from '../../../hooks/useFetch';
-import { setMockUseDispatchReturnValue } from '../../../test-utils/mockReactRedux';
-import { CredentialTypesPage } from '../../../pages/User/CredentialTypes/CredentialTypesPage';
+import {renderWithRouter} from '../../../test-utils/mockUtils';
+import {screen, waitFor} from '@testing-library/react';
+import {RequestStatus} from '../../../hooks/useFetch';
+import {setMockUseDispatchReturnValue} from '../../../test-utils/mockReactRedux';
+import {CredentialTypesPage} from '../../../pages/User/CredentialTypes/CredentialTypesPage';
+import {useApi} from "../../../hooks/useApi";
 
-jest.mock('../../../hooks/useFetch', () => ({
-  useFetch: jest.fn(),
-  RequestStatus: {
-    LOADING: 0,
-    DONE: 1,
-    ERROR: 2,
-  },
+jest.mock('../../../hooks/useApi.ts', () => ({
+    useApi: jest.fn(),
+    RequestStatus: {
+        LOADING: 0,
+        DONE: 1,
+        ERROR: 2,
+    },
 }));
 
 jest.mock('react-toastify', () => ({
-  toast: {
-    error: jest.fn(),
-  },
+    toast: {
+        error: jest.fn(),
+    },
 }));
 
 jest.mock('../../../components/Common/Buttons/NavBackArrowButton', () => ({
-  NavBackArrowButton: (props: { onClick?: () => void }) => (
-    <button data-testid="back-button" onClick={props.onClick}>
-      Back
-    </button>
-  ),
+    NavBackArrowButton: (props: { onClick?: () => void }) => (
+        <button data-testid="back-button" onClick={props.onClick}>
+            Back
+        </button>
+    ),
 }));
 
 jest.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: string) => key }),
-  initReactI18next: {
-    type: '3rdParty',
-    init: () => {},
-  },
+    useTranslation: () => ({t: (key: string) => key}),
+    initReactI18next: {
+        type: '3rdParty',
+        init: () => {
+        },
+    },
 }));
 
-const mockUseFetch = useFetch as jest.Mock;
 
 describe('CredentialTypesPage', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    setMockUseDispatchReturnValue(jest.fn());
-  });
-
-  it('renders CredentialTypesPage component', () => {
-    mockUseFetch.mockReturnValue({
-      state: RequestStatus.DONE,
-      fetchRequest: jest.fn(),
+    const mockUseApi = useApi as jest.Mock;
+    const fetchDataMock = jest
+        .fn()
+        .mockResolvedValueOnce({
+            data: {response: {display: [{language: 'en', name: 'Issuer1'}]}},
+            status: 200,
+            headers: {},
+            state: RequestStatus.DONE,
+            error: null
+        })
+        .mockResolvedValueOnce({
+            data: {response: []},
+            status: 200,
+            headers: {},
+            state: RequestStatus.DONE,
+            error: null
+        });
+    beforeEach(() => {
+        jest.clearAllMocks();
+        setMockUseDispatchReturnValue(jest.fn());
+        mockUseApi.mockReturnValue({state: RequestStatus.DONE, fetchData: fetchDataMock});
     });
 
-    renderWithRouter(<CredentialTypesPage backUrl="/user/home" />);
+    it('renders CredentialTypesPage component', () => {
+        mockUseApi.mockReturnValue({
+            state: RequestStatus.DONE,
+            fetchData: jest.fn(),
+        });
 
-    expect(screen.getByTestId('credential-types-page-container')).toBeInTheDocument();
-  });
+        renderWithRouter(<CredentialTypesPage backUrl="/user/home"/>);
 
-  it('matches snapshot after data loads', async () => {
-    const fetchMock = jest
-      .fn()
-      .mockResolvedValueOnce({
-        response: { display: [{ language: 'en', name: 'Issuer1' }] },
-      })
-      .mockResolvedValueOnce({ response: [] });
+        expect(screen.getByTestId('credential-types-page-container')).toBeInTheDocument();
+    });
 
-    mockUseFetch.mockReturnValue({ state: RequestStatus.DONE, fetchRequest: fetchMock });
+    it('matches snapshot after data loads', async () => {
+        const {asFragment} = renderWithRouter(<CredentialTypesPage/>);
 
-    const { asFragment } = renderWithRouter(<CredentialTypesPage />);
-
-    await waitFor(() => expect(fetchMock).toHaveBeenCalledTimes(2));
-    expect(screen.getByText('Issuer1')).toBeInTheDocument();
-    expect(asFragment()).toMatchSnapshot();
-  });
+        await waitFor(() => expect(fetchDataMock).toHaveBeenCalledTimes(2));
+        expect(screen.getByText('Issuer1')).toBeInTheDocument();
+        expect(asFragment()).toMatchSnapshot();
+    });
 
 });
--- inji-web/src/pages/User/CredentialTypes/CredentialTypesPage.tsx
+++ inji-web/src/pages/User/CredentialTypes/CredentialTypesPage.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useLocation, useNavigate, useParams} from 'react-router-dom';
-import {RequestStatus, useFetch} from '../../../hooks/useFetch';
+import {RequestStatus} from '../../../hooks/useFetch';
 import {useDispatch, useSelector} from 'react-redux';
 import {storeSelectedIssuer} from '../../../redux/reducers/issuersReducer';
 import {storeCredentials, storeFilteredCredentials} from '../../../redux/reducers/credentialsReducer';
 import {api} from '../../../utils/api';
-import {ApiRequest, IssuerObject, IssuerWellknownDisplayArrayObject} from '../../../types/data';
+import {ApiRequest, IssuerObject, IssuerWellknownDisplayArrayObject, ResponseTypeObject} from '../../../types/data';
 import {getIssuerDisplayObjectForCurrentLanguage} from '../../../utils/i18n';
 import {RootState} from '../../../types/redux';
 import {isObjectEmpty} from '../../../utils/misc';
@@ -15,6 +15,7 @@ import {useDownloadSessionDetails} from "../../../hooks/User/useDownloadSession"
 import {CredentialTypesPageContent} from "../../../components/User/CredentialTypes/CredentialTypesPageContent";
 import {Header} from "../../../components/User/CredentialTypes/Header";
 import {ROUTES} from "../../../utils/constants";
+import {useApi} from "../../../hooks/useApi";
 
 type CredentialTypesPageProps = {
     backUrl?: string;
@@ -23,7 +24,6 @@ type CredentialTypesPageProps = {
 export const CredentialTypesPage: React.FC<CredentialTypesPageProps> = ({
                                                                             backUrl
                                                                         }) => {
-    const {state, fetchRequest} = useFetch();
     const params = useParams<CredentialParamProps>();
     const dispatch = useDispatch();
     const language = useSelector((state: RootState) => state.common.language);
@@ -45,6 +45,8 @@ export const CredentialTypesPage: React.FC<CredentialTypesPageProps> = ({
     } = useDownloadSessionDetails();
 
     const [downloadStatus, setDownloadStatus] = useState<RequestStatus | null>(null);
+    const issuer = useApi<ResponseTypeObject>()
+    const issuersConfiguration = useApi<ResponseTypeObject>()
 
     useEffect(() => {
         const status = currentSessionDownloadId ? downloadInProgressSessions[currentSessionDownloadId]?.downloadStatus : null;
@@ -66,30 +68,28 @@ export const CredentialTypesPage: React.FC<CredentialTypesPageProps> = ({
     }, [downloadStatus])
 
     useEffect(() => {
-        if (state === RequestStatus.ERROR) {
+        if (issuersConfiguration.state === RequestStatus.ERROR || issuer.state === RequestStatus.ERROR) {
             setDownloadStatus(RequestStatus.ERROR);
         }
-    }, [state])
+    }, [issuersConfiguration.state, issuer.state])
 
     useEffect(() => {
         const fetchCall = async () => {
             let apiRequest: ApiRequest = api.fetchSpecificIssuer;
-            let response = await fetchRequest(
-                apiRequest.url(params.issuerId ?? ''),
-                apiRequest.methodType,
-                apiRequest.headers()
-            );
-            dispatch(storeSelectedIssuer(response?.response));
-            setSelectedIssuer(response?.response);
+            const {data: issuerResponse} = await issuer.fetchData({
+                url: apiRequest.url(params.issuerId ?? ''),
+                apiRequest: apiRequest
+            });
+            dispatch(storeSelectedIssuer(issuerResponse?.response));
+            setSelectedIssuer(issuerResponse?.response);
 
             apiRequest = api.fetchIssuersConfiguration;
-            response = await fetchRequest(
-                apiRequest.url(params.issuerId ?? ''),
-                apiRequest.methodType,
-                apiRequest.headers()
-            );
-            dispatch(storeFilteredCredentials(response?.response));
-            dispatch(storeCredentials(response?.response));
+            const {data: issuerConfigurationResponse} = await issuersConfiguration.fetchData({
+                url: apiRequest.url(params.issuerId ?? ''),
+                apiRequest: apiRequest
+            })
+            dispatch(storeFilteredCredentials(issuerConfigurationResponse?.response));
+            dispatch(storeCredentials(issuerConfigurationResponse?.response));
         };
         fetchCall();
     }, []);
@@ -113,7 +113,7 @@ export const CredentialTypesPage: React.FC<CredentialTypesPageProps> = ({
         >
             <Header onBackClick={handleBackClick} displayObject={displayObject}
                     onClick={() => navigateToUserHome(navigate)}/>
-            <CredentialTypesPageContent downloadStatus={downloadStatus} state={state}/>
+            <CredentialTypesPageContent downloadStatus={downloadStatus} state={issuersConfiguration.state}/>
         </div>
     );
 };
