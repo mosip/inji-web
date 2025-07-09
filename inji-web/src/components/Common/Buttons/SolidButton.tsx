2a718cfe221fdd1aea8ad051574e3b11839d3af6
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-07-09 10:31:18 +0530

    [INJIWEB-1627] make changes in solid button component to not to show hovering effect to the submit button in passcode page when it is in disabled state and resolve the onClick warning coming in solidButton and extract styles of it into separate file
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/components/Common/Buttons/SolidButton.tsx
+++ inji-web/src/components/Common/Buttons/SolidButton.tsx
@@ -1,20 +1,18 @@
 import React from "react";
+import {SolidButtonStyles} from "./SolidButtonStyles";
 
 export const SolidButton: React.FC<SolidButtonProps> = (props) => {
     return <div className={props.fullWidth ? "w-full" : ""}>
-        <div
-            className={`w-full bg-iw-primary text-center cursor-pointer items-center text-iw-text font-bold text-sm px-6 py-3 rounded-lg shadow hover:shadow-lg mr-1 ${props.disabled ? 'bg-gray-400 cursor-not-allowed' : ' bg-gradient-to-r from-iw-primary to-iw-secondary'} ${props.className || ''}`}
+        <button
+            data-testid={props.testId}
+            className={`${SolidButtonStyles.baseStyles} ${!props.disabled && 'hover:shadow-lg cursor-pointer'} ${props.disabled ? 'bg-gray-400 cursor-not-allowed' : ' bg-gradient-to-r from-iw-primary to-iw-secondary'} ${props.className || ''}`}
+            disabled={props.disabled}
             onClick={!props.disabled ? props.onClick : undefined}>
-            <button
-                data-testid={props.testId}
-                className="text-white font-bold rounded-lg"
-                disabled={props.disabled}>
-                <div className={"flex items-center justify-center gap-1.5"}>
-                    {props.icon && <span>{props.icon}</span>}
-                    <span>{props.title}</span>
-                </div>
-            </button>
-        </div>
+            <div className={"flex items-center justify-center gap-1.5"}>
+                {props.icon && <span>{props.icon}</span>}
+                <span>{props.title}</span>
+            </div>
+        </button>
     </div>
 }
 
