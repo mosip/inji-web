c004f2e59da7e6b0324a408c0e4ed4d9e267d5ed
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-08-12 17:07:32 +0530

    [Injiweb 1663] Update the docker compose config files to keep it consistent with mimoto docker compose folder files (#383)

--- inji-web/src/components/Preview/PDFViewerStyles.ts
+++ inji-web/src/components/Preview/PDFViewerStyles.ts
@@ -1,3 +1,4 @@
 export const PDFViewerStyles = {
-    'container': 'w-full max-h-screen relative'
+    'container': 'w-full max-h-screen relative flex flex-col items-center overflow-auto',
+    'pageWrapper': 'mb-2 border border-gray-200'
 };
