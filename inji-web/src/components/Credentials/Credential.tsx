12fdef8ec3f1be5ed8de00a0a24fd967bb9ac26a
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-08-20 10:49:13 +0530

    [INJIWEB-1701] add credential type as suffix to the testId of each credential type shown in the UI for issuer to make it easy for the automation team to fetch the specific credential type and initiate the download
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/components/Credentials/Credential.tsx
+++ inji-web/src/components/Credentials/Credential.tsx
@@ -95,6 +95,7 @@ export const Credential: React.FC<CredentialProps> = (props) => {
                                                     ? onSuccess(-1)
                                                     : setCredentialExpiry(true);
                 }}
+                testIdSuffix={filteredCredentialConfig.name}
             />
             {credentialExpiry && (
                 <DataShareExpiryModal
