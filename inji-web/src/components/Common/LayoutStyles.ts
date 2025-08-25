3b3986809de8217f69c352f63aa6865aadf88cb0
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-08-25 15:36:51 +0530

    [INJIWEB-1675] remove min height and fixed width to the layout page to properly show all the content of the children in the UI irrespective of whether page is zoomed in or zoomed out (#399)

--- inji-web/src/components/Common/LayoutStyles.ts
+++ inji-web/src/components/Common/LayoutStyles.ts
@@ -5,7 +5,7 @@ const LayoutStyles = {
     dashboardBgTop: "absolute top-0 left-0 w-full z-[-1]",
     dashboardBgBottom: "absolute bottom-0 left-0 w-full z-[-1]",
     outletWrapper: "flex flex-grow flex-col relative z-10 overflow-y-auto",
-    outletInner: "flex flex-col items-center justify-center min-h-full w-full",
+    outletInner: "flex flex-col items-center justify-center",
     sectionWithWhiteBg: "bg-white rounded-lg shadow-iw-layout p-6 sm:p-5 md:p-10 flex flex-col items-center justify-center min-h-[400px] sm:min-h-[450px] mt-4 sm:mt-8",
     successToastContainer: "bg-iw-darkgreen",
     errorToastContainer: "bg-iw-brightRed",
