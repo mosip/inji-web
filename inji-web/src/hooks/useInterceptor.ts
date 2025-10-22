4c94f8ddbf9d801fc2b79ef1d47e40f3acb98755
jaswanthkumarpolisetty <jaswanthkumar.p@thoughtworks.com>
2025-10-22 08:20:46 +0530

    add the testcases
    Signed-off-by: jaswanthkumarpolisetty <jaswanthkumar.p@thoughtworks.com>

--- inji-web/src/hooks/useInterceptor.ts
+++ inji-web/src/hooks/useInterceptor.ts
@@ -30,17 +30,20 @@ export function useInterceptor() {
         // Redirect to / page on logged-in user if unauthorized access is detected
         const currentRoute = location.pathname + location.search + location.hash;
         const isPasscodeRelatedRoute = location.pathname === ROUTES.USER_RESET_PASSCODE || location.pathname === ROUTES.USER_PASSCODE;
-        const isSessionActive: boolean = !!AppStorage.getItem(KEYS.USER);
-        if (error.response && error.response.status === 401) {
-            removeUser()
-            // Avoid redirecting to passcode related pages in case of unauthorized access and re-login. This avoids unnecessary redirections to passcode related pages. If this is not leads to below sort of scenario.
-            // Scenario: User authenticated via Provider -> tries to access passcode related page -> session expire & got unauthorized access
-            // -> redirect to log in -> user logs in again -> enter passcode -> submit -> user is redirected to passcode page again // Here user us required to enter passcode again, which is not required.
-            if (!isPasscodeRelatedRoute)
-                AppStorage.setItem(KEYS.REDIRECT_TO, currentRoute, true);
-            console.warn("Unauthorized access detected. Redirecting to / page.");
-            navigate(ROUTES.ROOT)
-        }
+        const isSessionActive: boolean = !!AppStorage.getItem(KEYS.USER);    
+            if (error.response && error.response.status === 401) {
+                removeUser()
+                // Avoid redirecting to passcode related pages in case of unauthorized access and re-login. This avoids unnecessary redirections to passcode related pages. If this is not leads to below sort of scenario.
+                // Scenario: User authenticated via Provider -> tries to access passcode related page -> session expire & got unauthorized access
+                // -> redirect to log in -> user logs in again -> enter passcode -> submit -> user is redirected to passcode page again // Here user us required to enter passcode again, which is not required.
+                if (!isPasscodeRelatedRoute)
+                {
+                    console.log("Hi")
+                    AppStorage.setItem(KEYS.REDIRECT_TO, currentRoute, true);
+                }    
+                console.warn("Unauthorized access detected. Redirecting to / page.");
+                navigate(ROUTES.ROOT)
+            }
         return Promise.reject(error as unknown as Error);
     });
 
