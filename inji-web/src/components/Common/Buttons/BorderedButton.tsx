a60d30696439341c7b4c88d722fa44b17737fa21
cyber-titan <saiabhi2309@gmail.com>
2025-10-24 16:10:28 +0530

    injiweb-1726 added test id for guest login (#444)
    Signed-off-by: cyber_titan <saiabhi2309@gmail.com>

--- inji-web/src/components/Common/Buttons/BorderedButton.tsx
+++ inji-web/src/components/Common/Buttons/BorderedButton.tsx
@@ -9,6 +9,7 @@ export const BorderedButton:React.FC<BorderedButtonProps> = (props) => {
     <div className={"bg-gradient-to-r text-center cursor-pointer from-iw-primary to-iw-secondary p-0.5 rounded-lg"} onClick={isDisabled ? undefined : props.onClick}>
             <div className={`py-1 px-2 bg-white justify-center center rounded-lg ${!isDisabled ? 'hover:bg-none hover:bg-[#FFF2F2] hover:text-[#E64E4E]' : ''} ${props.className}`}>
                 <button
+                    id={props.testId}
                     data-testid={props.testId}
                     className={`${BorderedButtonStyles.baseStyles } ${props.disabled ? BorderedButtonStyles.disabledClasses : ""}`}
                     disabled={props.disabled}>
